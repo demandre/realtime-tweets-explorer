31e56e46209acda5fbf9d16721663f46212fe734
demandre <joris.demandre@gmail.com>
2019-02-25 14:50:59 +0100

    add tweet limit to collection

--- src/collections/tweet.js
+++ src/collections/tweet.js
@@ -7,6 +7,9 @@ var Tweet = require('../models/tweet.js');
 var Tweets = Backbone.Collection.extend({
   'model': Tweet,
   'addTweet': function addTweet (tweet) {
+    if (this.length > 20) {
+      this.remove(this.at(0));
+    }
     this.add(new Tweet(tweet));
   }
 });
