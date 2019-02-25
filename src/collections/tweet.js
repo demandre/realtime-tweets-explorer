664fb2215f10e8db2d79c53465ee074f6fe60980
demandre <joris.demandre@gmail.com>
2019-02-25 12:42:59 +0100

    remove dummy api response

--- src/collections/tweet.js
+++ src/collections/tweet.js
@@ -2,32 +2,7 @@
 var Backbone = require('backbone');
 // Model
 var Tweet = require('../models/tweet.js');
-// Api response
-var response = [{
-  'createdAt': 'Tue Jan 15 10:22:02 +0000 2019',
-  'id_str': '1085119914376077314',
-  'text': 'We are hiring in the STATION F team! If you LOVE startups, with capital letters, and are looking for an internship,… https://t.co/1bqWJI5uDi',
-  'user': {
-    'name': 'STATION F',
-    'username': 'johndoe',
-    'profile_image_url': 'http://pbs.twimg.com/profile_images/937640813395431425/HfQbWe_1_normal.jpg'
-  },
-  'coordinates': null,
-  'retweet_count': 2,
-  'favorite_count': 12
-}, {
-  'createdAt': 'Tue Jan 15 09:48:46 +0000 2019',
-  'id_str': '1085111542725644288',
-  'text': 'Réparer l\'ascenseur social, s\'engager à tous les étages, soutenir l\'égalité des chances ! Bravo @BNPParibas… https://t.co/QHg9s2jb0R',
-  'user': {
-    'name': 'La France s\'engage',
-    'username': 'johndoe',
-    'profile_image_url': 'http://pbs.twimg.com/profile_images/879939336212537345/9s_xtRrJ_normal.jpg'
-  },
-  'coordinates': null,
-  'retweet_count': 4,
-  'favorite_count': 8
-}];
+
 // Collection
 var Tweets = Backbone.Collection.extend({
   'model': Tweet,
